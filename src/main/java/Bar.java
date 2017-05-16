c166f37f9d705866b74eff39b07dbe29eb068f7b
Gabriele Graffieti <gabriele.graffieti@studio.unibo.it>
2017-05-16 15:26:39 +0200

    fix for pull request

--- src/main/java/Bar.java
+++ src/main/java/Bar.java
@@ -1,8 +1,14 @@
 /**
- *  Created by gab on 16/05/17.
+ *  Wonderful class, that prints the string "Bar" on the console.
+ *  @author Gabriele Graffieti
+ *
  */
 public class Bar {
     public static void main(String[] args) {
-        System.out.println("Bar");
+        log("Bar");
+    }
+
+    private static void log(final String message) {
+        System.out.println(message);
     }
 }
